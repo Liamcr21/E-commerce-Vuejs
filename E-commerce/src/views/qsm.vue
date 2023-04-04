b7374786f5a4d92cc6aaad9304e5e62c7f994cd0
Liamcr <cariou.liam@orange.fr>
2023-04-04 16:00:28 +0200

    qui sommes nous ok

---
+++ E-commerce/src/views/qsm.vue
@@ -0,0 +1,39 @@
+<template>
+
+<div>
+    <navbar :active="active" @update:active="active = $event" />
+    <router-view />
+  </div>
+
+
+    <div>
+    <qsm/>
+    </div>
+
+    <div>
+      <footere/>
+    </div>
+  
+  
+</template>
+
+
+<script>
+import Navbar from '@/components/nav.vue';
+  import qsm from '@/components/qsm.vue';
+  import footere from '@/components/footer.vue';
+  
+  export default {
+      name: 'qui-sommes-nous',
+    components: {
+      Navbar,
+      qsm,
+      footere, 
+    },
+    data() {
+      return {
+        active: 'accueil',
+      };
+    },
+  };
+</script>
