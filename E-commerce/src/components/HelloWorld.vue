9f95669b9162036fd5e85c8639f9a9652fbac319
Liamcr <cariou.liam@orange.fr>
2023-04-04 11:06:33 +0200

    ajout bootstrap

--- E-commerce/src/components/HelloWorld.vue
+++ E-commerce/src/components/HelloWorld.vue
@@ -14,6 +14,8 @@ defineProps({
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <button class="btn btn-primary">Cliquez ici</button>
+
     </h3>
   </div>
 </template>
