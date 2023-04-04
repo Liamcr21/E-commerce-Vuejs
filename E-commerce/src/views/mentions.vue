a4cbcf35e802d8b26a44ae3473ad3b079de2c294
Liamcr <cariou.liam@orange.fr>
2023-04-04 16:01:08 +0200

    ajout des mentions légal

---
+++ E-commerce/src/views/mentions.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div>
+        <navbar :active="active" @update:active="active = $event" />
+        <router-view />
+      </div>
+    
+    
+        <div>
+        <mentions/>
+        </div>
+    
+        <div>
+          <footere/>
+        </div>
+      
+      
+    </template>
+    
+    
+    <script>
+    import Navbar from '@/components/nav.vue';
+      import mentions from '@/components/mentions.vue';
+      import footere from '@/components/footer.vue';
+      
+      export default {
+          name: 'mentionsl',
+        components: {
+          Navbar,
+          mentions,
+          footere, 
+        },
+        data() {
+          return {
+            active: 'accueil',
+          };
+        },
+      };
+    </script>
