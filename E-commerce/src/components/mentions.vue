017dc67451a1bae63ccd3bfa44659acfb8b188fd
Liamcr <cariou.liam@orange.fr>
2023-04-04 16:01:42 +0200

    component mentions

---
+++ E-commerce/src/components/mentions.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <h1 class="display-5 fw-bold">Mentions légals</h1>
+    Hawai Clothes est une marque déposée appartenant à [Nom de la société]. Tous les droits d'auteur, marques déposées et autres droits de propriété intellectuelle relatifs à notre site web et à son contenu sont la propriété de [Nom de la société] ou sont utilisés avec l'autorisation du propriétaire.
+
+Notre site web utilise des cookies pour améliorer l'expérience de navigation de nos utilisateurs. En utilisant notre site web, vous acceptez notre politique de cookies.
+
+[Nom de la société] est une entreprise enregistrée en France, dont le siège social est situé à [Adresse du siège social]. Nous sommes immatriculés au registre du commerce et des sociétés de [Ville d'immatriculation] sous le numéro [Numéro d'immatriculation].
+
+Notre entreprise est soumise à la TVA en France. Notre numéro de TVA est [Numéro de TVA].
+
+Si vous avez des questions concernant notre site web ou nos produits, vous pouvez nous contacter à l'adresse e-mail suivante : [Adresse e-mail de contact].
+</div>
+</template>
