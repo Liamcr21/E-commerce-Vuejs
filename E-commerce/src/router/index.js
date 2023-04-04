1dab167cd17be31a44b1ff592fda72f0bb8923b9
Liamcr <cariou.liam@orange.fr>
2023-04-04 15:33:15 +0200

    mise en place des routes

--- E-commerce/src/router/index.js
+++ E-commerce/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/index.vue'
+import HomeView from '../views/index.vue';
+import accueil from '../views/index.vue';
+import contact from '../views/contact.vue';
+import boutique from '../views/boutique.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,28 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/contact',
+      name: 'contact',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/contact.vue')
+    },
+    {
+      path: '/accueil',
+      name: 'accueil',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/index.vue')
+    },
+    {
+      path: '/boutique',
+      name: 'boutique',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/boutique.vue')
     }
   ]
 })
