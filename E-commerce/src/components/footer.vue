dcebb1c1565d64fd128c6c40fe9634e3e268db36
Liamcr <cariou.liam@orange.fr>
2023-04-04 14:47:13 +0200

    component footer

---
+++ E-commerce/src/components/footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card-f2">
+    <div class="card  text-center">
+  <div class="card-body">
+    <h5 class="card-title">Hawaï Clothes</h5>
+    <p class="card-text">Boutique de vétements 100% francais</p>
+    <a href="#" class="btn btn-outline-info">Mentions Légals</a>
+  </div>
+  <div class="card-footer text-muted">
+    @Tous droit réservés - 2023 - Création par Liam Cariou et Bogdan Monoté
+  </div>
+</div>
+</div>
+</template>
+
+<style>
+.card-f2 {
+  margin-top: 2%;
+  bottom: 0;
+  width: 100%;
+}
+
+
+</style>
