eb20b0c23d7f3f718e2926456bde42750cb41d59
Liamcr <cariou.liam@orange.fr>
2023-04-04 11:21:50 +0200

    navbar

---
+++ E-commerce/src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <mon-composant></mon-composant>
+    </div>
+  </template>
+  
+  <script>
+  import MonComposant from '@/components/nav.vue'
+  
+  export default {
+    name: 'MaVue',
+    components: {
+      MonComposant
+    },
+    // Autres options de votre vue
+  }
+  </script>
