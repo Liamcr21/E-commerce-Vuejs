3827d27f8cc1176d9577e1024710141604d1bd92
Liamcr <cariou.liam@orange.fr>
2023-04-04 14:47:38 +0200

    ajout des components

--- E-commerce/src/views/index.vue
+++ E-commerce/src/views/index.vue
@@ -1,35 +1,34 @@
-<!-- <template>
-    <div>
-      <navbar></navbar>
-    </div>
-  </template>
-  
-  <script>
-  import navbar from '@/components/nav.vue'
-  
-  export default {
-    name: 'maNavbar',
-    components: {
-      navbar
-    },
-    // Autres options de votre vue
-  }
-  </script> -->
+
 
 <template>
   <div>
     <navbar :active="active" @update:active="active = $event" />
     <router-view />
   </div>
+
+  <div>
+    <AccProduit/>
+  </div>
+
+  <div>
+    <footere/>
+  </div>
+
+
 </template>
 
 <script>
 import Navbar from '@/components/nav.vue';
+import AccProduit from '@/components/accproduit.vue';
+import footere from '@/components/footer.vue';
 
 export default {
+    name: 'home',
   name:'maNavbar',
   components: {
     Navbar,
+    AccProduit,
+    footere, 
   },
   data() {
     return {
