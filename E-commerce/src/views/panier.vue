54639778a597c5dfa4268815774c95fa6cb9eddd
Liamcr <cariou.liam@orange.fr>
2023-04-05 10:16:40 +0200

    panier

---
+++ E-commerce/src/views/panier.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div>
+        <navbar :active="active" @update:active="active = $event" />
+        <router-view />
+      </div>
+    
+    
+        <div>
+        <panier/>
+        </div>
+    
+        <div>
+          <footere/>
+        </div>
+      
+      
+    </template>
+    
+    
+    <script>
+    import Navbar from '@/components/nav.vue';
+      import panier from '@/components/panier.vue';
+      import footere from '@/components/footer.vue';
+      
+      export default {
+          name: 'paniers',
+        components: {
+          Navbar,
+          panier,
+          footere, 
+        },
+        data() {
+          return {
+            active: 'accueil',
+          };
+        },
+      };
+    </script>
