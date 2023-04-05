89dba77f8114511deee0e129a36300c2d0175d4b
Liamcr <cariou.liam@orange.fr>
2023-04-05 14:00:10 +0200

    produit

--- E-commerce/src/router/index.js
+++ E-commerce/src/router/index.js
@@ -60,6 +60,15 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/panier.vue')
     }
+    ,
+    {
+      path: '/produit',
+      name: 'produits',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/produit.vue')
+    }
   ]
 })
 
